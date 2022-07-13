8a08337702a508f26b51e218791ccebf06bbb6f2
Maurício Kobren <mauriciokobren@gmail.com>
2022-07-13 17:17:45 -0300

    Removing token to call the api

--- definitions.py
+++ definitions.py
@@ -2,7 +2,3 @@ base_url = "https://api.github.com/"
 repo_owner = "mauriciokobren"
 repo_end_point = base_url  + "users/" + repo_owner + "/repos"
 
-github_payload = {}
-github_headers = {
-  'Authorization': 'Bearer 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'
-}
