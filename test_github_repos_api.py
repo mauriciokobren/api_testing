e8cd0f33cfe6383a8da484041b226993280582f6
Maurício Kobren <mauriciokobren@gmail.com>
2022-07-13 17:06:45 -0300

    Adding test files

---
+++ test_github_repos_api.py
@@ -0,0 +1,34 @@
+import requests
+import definitions
+import logging
+
+class  TestGithubReposAPI:
+    EXPECTED_NUMBER_OF_REPOS = 1
+    EXPECTED_REPO_NAME = 'api_testing'
+    EXPECTED_ELEMENTS = ['name','description','html_url','abc']
+
+    def setup_method(self, test_method):
+        self.response = requests.get(definitions.repo_end_point,headers=definitions.github_headers,
+         data = definitions.github_payload)
+
+    def test_status_is_200(self):
+        LOGGER = logging.getLogger(__name__)
+        if self.response.status_code != 200:
+            LOGGER.info('Status code is ' + str(self.response.status_code))
+        assert self.response.status_code == 200
+
+    def test_number_of_repos(self):
+        assert len(self.response.json()) == self.EXPECTED_NUMBER_OF_REPOS
+
+    def test_find_repo_in_response(self):
+        assert self.EXPECTED_REPO_NAME in str(self.response.json())
+
+    def test_basic_elements_in_response(self):
+        LOGGER = logging.getLogger(__name__)
+        found = True
+        for element in self.EXPECTED_ELEMENTS:
+            if not element in str(self.response.json()):
+                found = False
+                LOGGER.info('Element not found in response: ' + element)
+        assert found == True
+
